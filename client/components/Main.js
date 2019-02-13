276b816fd0b58e3195266a17fd00380423f4954a
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-02-13 21:06:26 +0100

    app layout and component setup

---
+++ client/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link>Reduxtagram</Link>
+        </h1>
+      </div>
+    )
+  }
+});
+
+export default Main;
